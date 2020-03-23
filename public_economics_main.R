11430b07b7c1857b84863f3fc4a2b3836ec18ba2
Osi Tavonrn <ossi.e.tahvonen@aalto.fi>
2020-03-23 23:54:49 +0200

    added csv, R now loads data

--- public_economics_main.R
+++ public_economics_main.R
@@ -1,1 +1,10 @@
 #this is the main file
+
+#packages
+library(MASS)
+#setting working directory
+goalwd <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(goalwd)
+#load data
+data <- read.csv2("MonthlyPanel.csv", header = T)
+View(data)
